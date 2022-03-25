09702daac2532b8b18f24392dc1dff98199e2245
Clovis NZOUENDJOU <clovis@ebusiness-cameroun.com>
2022-03-25 01:14:48 +0100

    Fix User schemas and add mail schema

--- schemas/mails.py
+++ schemas/mails.py
@@ -5,3 +5,7 @@ from pydantic import BaseModel, EmailStr
 
 class EmailSchema(BaseModel):
     email: List[EmailStr]
+
+
+class MessageSchema(BaseModel):
+    message: str
--- schemas/users.py
+++ schemas/users.py
@@ -1,6 +1,5 @@
-import uuid
 from datetime import datetime
-from typing import List, Optional
+from typing import Optional
 
 from pydantic import UUID4, BaseModel, EmailStr, Field
 
@@ -22,30 +21,44 @@ class CreateUpdateDictModel(BaseModel):
 class BaseUser(CreateUpdateDictModel):
     """Base User model."""
 
-    id: UUID4 = Field(default_factory=uuid.uuid4)
     email: EmailStr
-    is_active: bool = True
+    is_active: Optional[bool] = True
     is_verified: bool = False
 
 
-class UserCreate(CreateUpdateDictModel):
+class UserCreate(BaseUser):
+    """Create User model"""
+
     email: EmailStr
     password: str
+
+
+# Properties to receive via API on update
+class UserUpdate(CreateUpdateDictModel):
+    """Update User model"""
+
     is_active: Optional[bool] = True
+    password: Optional[str] = None
 
 
-class UserValidation(CreateUpdateDictModel):
-    email: EmailStr
-    is_active: Optional[bool] = True
-    is_verified: Optional[bool] = True
-    is_validation_mail_send: Optional[bool] = True
-    validation_date: Optional[datetime] = datetime.now()
+# In some case, we can verified users without sending email (eg. existing users)
+class UserValidation(BaseUser):
+    is_validation_mail_send: bool = True
+    validation_date: datetime = datetime.now()
 
 
-class UserShow(BaseModel):
+# To return via api (token)
+class UserAPI(BaseUser):
+    id: Optional[int] = None
+
+    class Config:
+        orm_mode = True
+
+
+class UserShow(BaseUser):
     email: EmailStr
     is_active: bool
-    is_verified: bool = False
+    is_verified: Optional[bool] = False
 
     class Config:
         orm_mode = True
