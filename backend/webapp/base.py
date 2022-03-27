b0b4f163e559fb202818c461cc4632bbb85f51ea
Clovis NZOUENDJOU <clovis@ebusiness-cameroun.com>
2022-03-27 22:05:33 +0200

    REMOVE WEBAPP The objective of the webapp module was to set up a user interface allowing to test the API set up from a web application. Given the time allowed (and especially because it was not explicitly requested in the statement of this test)

--- backend/webapp/base.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-from backend.webapp.auth import route_logins
-from backend.webapp.users import route_users
-
-webapps_router = APIRouter(include_in_schema=False)
-
-webapps_router.include_router(router=route_users.router, prefix="", tags=["Sign Up"])
-webapps_router.include_router(router=route_logins.router, prefix="", tags=["Sign In"])
